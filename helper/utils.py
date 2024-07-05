1b1fe17153efa659faddb623d3a5486225d93710
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-05 09:16:23 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -1,58 +1,46 @@
-import math
-import time
+import math, time
 from datetime import datetime
 from pytz import timezone
-from config import Config, Txt
+from config import Config, Txt 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 
 async def progress_for_pyrogram(current, total, ud_type, message, start):
     now = time.time()
     diff = now - start
-
-    if round(diff % 5.00) == 0 or current == total:
+    if round(diff % 5.00) == 0 or current == total:        
         percentage = current * 100 / total
         speed = current / diff
-
-        # Calculate elapsed time in milliseconds
         elapsed_time = round(diff) * 1000
-
-        # Calculate estimated time to completion in milliseconds
         time_to_completion = round((total - current) / speed) * 1000
-
-        # Calculate total estimated time in milliseconds
         estimated_total_time = elapsed_time + time_to_completion
 
-        # Format times into human-readable format
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        # Generate progress bar
         progress = "{0}{1}".format(
             ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
             ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))])
-        )
-
-        # Construct progress message
-        tmp = progress + Txt.PROGRESS_BAR.format(
+        )            
+        tmp = progress + Txt.PROGRESS_BAR.format( 
             round(percentage, 2),
             humanbytes(current),
             humanbytes(total),
-            humanbytes(speed),
+            humanbytes(speed),            
             estimated_total_time if estimated_total_time != '' else "0 s"
         )
-
         try:
             await message.edit(
-                text=f"{ud_type}\n\n{tmp}",
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="close")]])
+                text=f"{ud_type}\n\n{tmp}",               
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✖️ 𝙲𝙰𝙽𝙲𝙴𝙻 ✖️", callback_data="close")]])                                               
             )
         except:
             pass
 
-def humanbytes(size):
+def humanbytes(size):    
     if not size:
         return ""
-    power = 2 ** 10
+    power = 2**10
     n = 0
     Dic_powerN = {0: ' ', 1: 'K', 2: 'M', 3: 'G', 4: 'T'}
     while size > power:
@@ -60,27 +48,25 @@ def humanbytes(size):
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'ʙ'
 
+
 def TimeFormatter(milliseconds: int) -> str:
     seconds, milliseconds = divmod(int(milliseconds), 1000)
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
-
     tmp = ((str(days) + "ᴅ, ") if days else "") + \
-          ((str(hours) + "ʜ, ") if hours else "") + \
-          ((str(minutes) + "ᴍ, ") if minutes else "") + \
-          ((str(seconds) + "ꜱ, ") if seconds else "") + \
-          ((str(milliseconds) + "ᴍꜱ, ") if milliseconds else "")
-
-    return tmp[:-2]
+        ((str(hours) + "ʜ, ") if hours else "") + \
+        ((str(minutes) + "ᴍ, ") if minutes else "") + \
+        ((str(seconds) + "ꜱ, ") if seconds else "") + \
+        ((str(milliseconds) + "ᴍꜱ, ") if milliseconds else "")
+    return tmp[:-2] 
 
 def convert(seconds):
     seconds = seconds % (24 * 3600)
     hour = seconds // 3600
     seconds %= 3600
     minutes = seconds // 60
-    seconds %= 60
-
+    seconds %= 60      
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 async def send_log(b, u):
@@ -88,9 +74,8 @@ async def send_log(b, u):
         curr = datetime.now(timezone("Asia/Kolkata"))
         date = curr.strftime('%d %B, %Y')
         time = curr.strftime('%I:%M:%S %p')
-
         await b.send_message(
             Config.LOG_CHANNEL,
             f"**--Nᴇᴡ Uꜱᴇʀ Sᴛᴀʀᴛᴇᴅ Tʜᴇ Bᴏᴛ--**\n\nUꜱᴇʀ: {u.mention}\nIᴅ: `{u.id}`\nUɴ: @{u.username}\n\nDᴀᴛᴇ: {date}\nTɪᴍᴇ: {time}\n\nBy: {b.mention}"
         )
-
+        
