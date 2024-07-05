763c2d0770c4ebb65796c54df79217fecfe7c97c
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-05 08:43:20 +0530

    Update Force_Sub.py

--- plugins/Force_Sub.py
+++ plugins/Force_Sub.py
@@ -1,59 +1,52 @@
-"""
-Apache License 2.0
-Copyright (c) 2022 @PYRO_BOTZ 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-Telegram Link : https://t.me/PYRO_BOTZ 
-Repo Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT
-License Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT/blob/main/LICENSE
-"""
-
-from pyrogram import Client, filters, enums 
+# Import necessary modules
+from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import UserNotParticipant
-from config import Config
-from helper.database import db
+from config import Config  # Assuming Config contains your bot configuration
+from helper.database import db  # Assuming db handles database operations
 
+# Define a function to check if user is subscribed
 async def not_subscribed(_, client, message):
+    # Add user to database
     await db.add_user(client, message)
+    
+    # Check if forced subscription is required
     if not Config.FORCE_SUB:
         return False
-    try:             
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id) 
+    
+    try:
+        # Check if user is a member of the required channel or group
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
+        
+        # If user is banned, return True
         if user.status == enums.ChatMemberStatus.BANNED:
-            return True 
+            return True
         else:
-            return False                
+            return False
+    
     except UserNotParticipant:
         pass
+    
+    # Return True if user is not a participant
     return True
 
-
+# Define handler for private messages that are not subscribed
 @Client.on_message(filters.private & filters.create(not_subscribed))
 async def forces_sub(client, message):
-    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{Config.FORCE_SUB}") ]]
-    text = "**Sᴏʀʀy Dᴜᴅᴇ Yᴏᴜ'ʀᴇ Nᴏᴛ Jᴏɪɴᴇᴅ My Cʜᴀɴɴᴇʟ 😐. Sᴏ Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Uᴩᴅᴀᴛᴇ Cʜᴀɴɴᴇʟ Tᴏ Cᴄᴏɴᴛɪɴᴜᴇ**"
+    # Prepare message and buttons for non-subscribed users
+    buttons = [[InlineKeyboardButton(text="📢 Join Update Channel 📢", url=f"https://t.me/{Config.FORCE_SUB}")]]
+    text = "**Sorry, you're not joined my channel 😐. Please join our update channel to continue**"
+    
     try:
-        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)    
-        if user.status == enums.ChatMemberStatus.BANNED:                                   
-            return await client.send_message(message.from_user.id, text="Sᴏʀʀy Yᴏᴜ'ʀᴇ Bᴀɴɴᴇᴅ Tᴏ Uꜱᴇ Mᴇ")  
-    except UserNotParticipant:                       
+        # Check if user is banned from using the bot
+        user = await client.get_chat_member(Config.FORCE_SUB, message.from_user.id)
+        
+        if user.status == enums.ChatMemberStatus.BANNED:
+            return await client.send_message(message.from_user.id, text="Sorry, you're banned to use me")
+    
+    except UserNotParticipant:
+        # If user is not a participant, send subscription message with buttons
         return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
+    
+    # Send subscription message with buttons
     return await message.reply_text(text=text, reply_markup=InlineKeyboardMarkup(buttons))
-          
-
-
-
