d5aa522eaa405d697a0b8271f51af0427441fef8
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-05 08:18:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ class Config(object):
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5821871362').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "-1001864683653") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001870015374"))
 
